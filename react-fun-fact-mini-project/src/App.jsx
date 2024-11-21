b7071ef450d44d7800b022f6c30bbba448999c1d
Qadir Hassan <qadir.hassan333@gmail.com>
2024-11-21 13:51:40 +0500

    Added Header and Footer Component

--- react-fun-fact-mini-project/src/App.jsx
+++ react-fun-fact-mini-project/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+      <img src="src/assets/react.svg" alt="React Logo" />
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+      
+      </ul>
+    </div>
+  </div>
+</nav>
+     </header>
+     <CustomComponent />
+     <FooterComponent />
     </>
   )
 }
 
 export default App
+
+
+function CustomComponent(){
+  return (
+    <main>
+      <div className="card mt-5 col-lg-6 center m-auto">
+  <div className="card-header">
+    Fun Fact About React
+  </div>
+  <div className="card-body">
+    <ul>
+      <li>Virtual DOM: React uses a virtual copy of the Document Object Model (DOM) to render pages faster.</li>
+      <li>Component-based approach: React's component-based approach can be used to build cross-platform apps for web and mobile.</li>
+      <li>Easy to learn: Programmers with a good understanding of JavaScript can learn React in a few days</li>
+      <li>Supports type systems: React has a built-in way to validate props called propTypes.</li>
+    </ul>
+  </div>
+</div>
+    </main>
+  )
+}
+
+function FooterComponent(){
+  return (
+    <footer className='bg-white p-2 mt-5 text-dark d-flex justify-content-center text-center'>
+      <p className='text-center'>
+      &copy; All Right Reserved , Develop By Qadir Hassan
+      </p>
+   
+    </footer>
+  )
+}
